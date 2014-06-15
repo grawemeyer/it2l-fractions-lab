4d3694a830a405c1d5e92e6a64e8379610f08a73
Beate Grawemeyer <b.grawemeyer@gmail.com>
2014-06-15 17:33:24 +0100

    added rules

--- Assets/Standard Assets/Scripts/taskDependentSupport/core/Reasoning.cs
+++ Assets/Standard Assets/Scripts/taskDependentSupport/core/Reasoning.cs
@@ -13,6 +13,10 @@ namespace taskDependentSupport.core
 
 			bool correctSolution = false;
 			bool correctDenominator = false;
+			bool misconception1 = false;
+			bool misconception2 = false;
+			bool misconception3 = false;
+			bool misconception4 = false;
 			for (int i = 0; i < StudentModel.getCurrentFractions().Count; i++){
 				Fraction currentFraction = StudentModel.getCurrentFractions()[i];
 				int nominator = currentFraction.getNominator();
@@ -23,16 +27,39 @@ namespace taskDependentSupport.core
 					nominator = nominator * partition;
 					denominator = denominator * partition;
 				}
-				if (denominator == 12) {
+
+				if((denominator == 0) && (nominator == 0)){
+					misconception3 = true;
+				}
+
+				else if (denominator == 12) {
 					correctDenominator = true;
 					if (nominator == 9){
 						correctSolution = true;
 					}
+					else if (nominator == 3){
+						misconception1 = true;
+					}
+				}
+				else if (nominator == 12){
+					misconception4 = true;
+				}
+				else {
+					misconception2 = true;
 				}
 
 
 				if (correctSolution){
 					FeedbackStrategyModel.setMessage(2, "high");
+				}
+				else if (misconception1){
+					FeedbackStrategyModel.setMessage(4, "high");
+				}
+				else if (misconception2){
+					FeedbackStrategyModel.setMessage(5, "high");
+				}
+				else if (misconception3){
+					FeedbackStrategyModel.setMessage(6, "high");
 				}
 				else {
 					FeedbackStrategyModel.setMessage(0, "low");
