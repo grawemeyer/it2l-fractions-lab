5eff991eaa4f3c0c9483b35e9eb958433f6e675d
Matteo Romagnoli <matteoromagnoli@gmail.com>
2015-05-28 18:26:15 +0200

    Fix

--- Assets/Scripts/Behaviours/InterfaceBehaviour.cs
+++ Assets/Scripts/Behaviours/InterfaceBehaviour.cs
@@ -10,7 +10,7 @@ using System;
 
 public class InterfaceBehaviour : MonoBehaviour
 {
-    public const string VER = "0.304";
+    public const string VER = "0.306";
     public float min_orthographicsize = 10.0f;
 
     #region Protected Fields
